99924e127a6bed3be5a96f12d356a2cc261cc69f
joseph941230 <joseph941230@gmail.com>
2022-10-30 22:21:20 -0400

    First

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import logo from './logo.svg';
 import './App.css';
+import { EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
+import { UserOutlined,LockOutlined } from '@ant-design/icons';
+import { Input,Space,Button,Card, message, Result } from 'antd';
+import React, { useState } from 'react';
+import Password from 'antd/lib/input/Password';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  function App() {
+    let [userName,setUserName] = useState();
+    let [userPass,setUserPass] = useState();
+
+    let handleLogin = () => {
+      console.log(userName)
+      console.log(userPass)
+
+      if(!userName){
+         message.warning("invild User");
+      }
+      if(!userPass){
+        message.warning("invild Pass");
+     }
+ let url = "http://localhost:8080/user/login?phone=123&email=" + userName + "&password=" + userPass; 
+   fetch(url, {
+   method: 'POST',
+   mode:'no-cors'
+}).then(result => console.log("Server resp"+ result));
+  
+
+
+    }
+
+    return(
+  <div className="site-card-border-less-wrapper">
+    <Card title="Login">
+    <Space direction="vertical">
+      <Input size="default size" 
+      placeholder="User name" 
+      prefix={<UserOutlined />}
+      value = {userName}
+      onChange= {
+        
+        (e)=>{console.log(e.target.value)
+          setUserName(e.target.value)}
+        }
+       />
+      
+    <Input.Password 
+    prefix = {<LockOutlined />}
+      placeholder="Password"
+      iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+      value = {userPass}
+      onChange= {
+        
+        (e)=>{console.log(e.target.value)
+          setUserPass(e.target.value)}
+        }
+    />
+    <Button type="primary"onClick={handleLogin}>Login</Button>
+  </Space>
+    </Card>
+  </div>
+);}
 export default App;
